3ae7708adb97510ba1f48884d7f8ed5039ea2de6
MayankGupta <mayank.gupta.6.88@gmail.com>
2023-08-14 12:00:21 +0530

    Updated code for server

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
+// var httpServer = require("http");
+
+// var server = httpServer.createServer(function (req, res) {
+//     if (req.url == "/") {
+//         res.end("Hello TechnoFunnel");
+//     }
+    
+//     if (req.url == "/home") {
+//         res.end("Hello To Home");
+//     }
+    
+//     res.end("Hello. You are on Wrong Page");
+// })
+
+// server.listen(3000);
+
 var httpServer = require("http");
 
+var employees = [
+    { name: "A", id: 1 },
+    { name: "C", id: 2 }
+]
+
 var server = httpServer.createServer(function (req, res) {
+    var response = "Page Not Found";
+    if (req.url == "/employee/1") {
+        var returnData = employees.filter((employee) => {
+            return employee.id == 1
+        })
+        
+        res.end(JSON.stringify(returnData))
+    } 
+    if (req.url == "/employee/2") {
+        var returnData = employees.filter((employee) => {
+            return employee.id == 2
+        })
+        res.end(JSON.stringify(returnData))
+    } 
     if (req.url == "/") {
-        res.end("Hello TechnoFunnel");
-    }
-    
-    if (req.url == "/home") {
-        res.end("Hello To Home");
-    }
-    
-    res.end("Hello. You are on Wrong Page");
+        response = "Hello Mayank"
+    }   
+    if (req.url == "/contact") {
+        response = "Contact Page"
+    }  
+    if (req.url == "/about") {
+        response = "About Page"
+    }      
+    res.end(response)
 })
 
-server.listen(3000);
+server.listen(3000)
