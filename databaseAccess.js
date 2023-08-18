14f695fd96afd795dfc71317101dc4fc13159592
MayankGupta <mayank.gupta.6.88@gmail.com>
2023-08-18 10:53:22 +0530

    Added database connection

---
+++ databaseAccess.js
@@ -0,0 +1,39 @@
+var connectionString = `mongodb+srv://technofunnel:A110cate@cluster0.qoxqe.mongodb.net/?retryWrites=true&w=majority`;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+var fs = require("fs");
+
+const mongoClient = new MongoClient(connectionString,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+mongoClient.connect(connectionString).then(
+    (dbConnection) => {
+        var database = dbConnection.db("employees");
+        var employeeTable = database.collection("details");
+        
+        employeeTable.insertOne({
+            name: "Varun", age: 10
+        }).then((response) => {
+            console.log("User Added Varun")
+        });
+        
+        fs.readFile("./employees.json", function (err, data) {
+            var empList = JSON.parse(data.toString());
+            
+            empList.forEach(employee => {
+                employeeTable.insertOne({
+                    name: employee.name, age: employee.age
+                }).then((response) => {
+                    console.log("User Added " + employee.name)
+                });
+            });
+        })
+        
+    },
+    (err) => { }
+)
